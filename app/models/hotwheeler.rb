e9b0a0226e2246858d6b03d62d1b1efab7fe9a11
rhaynes3k <rhaynes3k@gmail.com>
2020-03-19 22:16:49 -0500

    added validations

--- app/models/hotwheeler.rb
+++ app/models/hotwheeler.rb
@@ -1,5 +1,6 @@
 class Hotwheeler < ActiveRecord::Base
   has_secure_password
+  validates :name, :username, :password, :age, presence: true
+  validates :username, :name, uniqueness: true
   has_many :cars
-
 end
